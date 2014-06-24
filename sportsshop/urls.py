216e590cea1f320411d0fb308c4434d0c4471b7b
root <root@vagrant-centos65.vagrantup.com>
2014-06-24 08:57:23 +0000

    DBにMySQLを設定した

--- sportsshop/urls.py
+++ sportsshop/urls.py
@@ -5,8 +5,11 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'sportsshop.views.home', name='home'),
+    #url(r'^$', 'sportsshop.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'^polls/', include('polls.urls')),
+
 )
