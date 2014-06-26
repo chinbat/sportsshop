d7ec3a75073cd913090fe5c037a5de8bb98e704c
root <root@vagrant-centos65.vagrantup.com>
2014-06-26 18:47:54 +0000

    Zuragnuud n dan ijil ch gesen, ymar ch gsn garj baigaa

---
+++ listing/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from listing.models import Post
+
+#def show(request, post_id):
+#    return HttpResponse('This is post %s', % post_id)
+def show(request, post_id):
+    return HttpResponse('This is post %s' % post_id)
+
+def index(request):
+    posts = Post.objects.all()
+    template = loader.get_template('listing/index.html')
+    context = RequestContext(request, {'posts': posts,})
+    return HttpResponse(template.render(context))
