bd7b690512dcb7b2317ea47c26437daf2c335bef
root <root@vagrant-centos65.vagrantup.com>
2014-06-27 06:08:23 +0000

    全部で6つのポストをshellの方から登録した。また、staticファイルも準備している。

--- listing/models.py
+++ listing/models.py
@@ -6,5 +6,5 @@ class Post(models.Model):
     pub_date = models.DateTimeField()
     posted_by = models.CharField(max_length=50)
     def __unicode__(self):
-        return self.post_text
+        return self.image
 # Create your models here.
--- listing/views.py
+++ listing/views.py
@@ -7,7 +7,8 @@ from listing.models import Post
 #def show(request, post_id):
 #    return HttpResponse('This is post %s', % post_id)
 def show(request, post_id):
-    return HttpResponse('This is post %s' % post_id)
+    post = Post.objects.get(id=post_id)
+    return HttpResponse('This is post: %s' % post.post_text)
 
 def index(request):
     posts = Post.objects.all()
